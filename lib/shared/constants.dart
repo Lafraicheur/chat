880769bf6bce73d15158117031eaf6793f8c375b
Lafraicheur <86606431+Lafraicheur@users.noreply.github.com>
2023-04-27 11:51:27 +0000

    Add files via upload

---
+++ lib/shared/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:409204931225:web:653b563bc84b240abdd608";
+  static String apiKey = "AIzaSyBYJa6wsrIDuzu9DkhKcuCp8bkiU2kviRU";
+  static String messagingSenderId = "409204931225";
+  static String projectId = "chatappflutter-98c9d";
+  final primaryColor = Color.fromARGB(255, 62, 13, 239);
+}
